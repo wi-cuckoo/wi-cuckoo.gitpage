214bcbbf6a20996cef1abe27adee03b5d91c33af
wei.liu <wei.liu@husor.com>
2017-08-18 17:09:50 +0800

    init repository

---
+++ nuxt.config.js
@@ -0,0 +1,50 @@
+
+const { join } = require('path')
+
+module.exports = {
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: 'cuckoo',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Alive Fish' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#3B8070' },
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** Run ESLINT on save
+    */
+    extend (config, ctx) {
+      if (ctx.dev && ctx.isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    },
+    /*
+    ** Vuetify Plugin
+    */
+    vendor: ['vuetify']
+  },
+
+  plugins: ['~plugins/vuetify.js'],
+  css: [
+    { src: join(__dirname, 'assets/css/app.styl'), lang: 'styl' }
+  ]
+}
