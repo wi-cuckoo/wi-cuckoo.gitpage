06dfc10446322c5a451b99b2aef7e9be50154335
Louis Wei <wi@localhost.localdomain>
2017-09-03 00:22:21 +0800

    change default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,43 @@
 <template>
   <div>
     <v-app light toolbar>
-      <v-toolbar class="white elevation-0 pl-1 pr-1" fixed light>
+      <!-- <v-toolbar class="white elevation-0 pl-1 pr-1" fixed light>
         <v-avatar class="blue" size="32px">
            <img src="/img/favicon.png" alt="Wi">
         </v-avatar>
         <v-spacer></v-spacer>
         <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+      </v-toolbar> -->
+      <v-navigation-drawer temporary v-model="drawer" right>
+        <v-list class="pa-1">
+          <v-list-tile avatar>
+            <v-list-tile-avatar>
+              <img src="/img/profile.jpg" />
+            </v-list-tile-avatar>
+            <v-list-tile-content>
+              <v-list-tile-title>Louis Wei</v-list-tile-title>
+            </v-list-tile-content>
+            </v-list-tile-action>
+          </v-list-tile>
+        </v-list>
+        <v-list class="pt-0" dense>
+          <v-divider></v-divider>
+          <v-list-tile v-for="item in items" :key="item.title" :href="item.href">
+            <v-list-tile-action>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </v-list>
+      </v-navigation-drawer>
+      <v-toolbar fixed class="white elevation-0" dark>
+        <v-avatar class="blue" size="32px">
+           <img src="/img/favicon.png" alt="Wi">
+        </v-avatar>
+        <v-spacer></v-spacer>
+        <v-toolbar-side-icon @click.stop="drawer = !drawer" class="grey--text"></v-toolbar-side-icon>
       </v-toolbar>
       <nuxt/>
     </v-app>
@@ -17,7 +48,12 @@
   export default {
     data () {
       return {
-        drawer: false
+        drawer: false,
+        items: [
+          { title: 'Home', icon: 'home', href: '/' },
+          { title: 'Blog', icon: 'note', href: '/blog' },          
+          { title: 'C V', icon: 'portrait', href: '/resume' }
+        ],
       }
     }
   }
